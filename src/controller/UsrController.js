e62e0386da2b97394130230256dd87127a259116
Sumit Sharma <sumit.sharma@ihg.com>
2021-09-03 15:50:17 +0530

    new apis

--- src/controller/UsrController.js
+++ src/controller/UsrController.js
@@ -44,32 +44,32 @@ const usr_add= async (req,res) =>{
 };
 
 
-const usr_update= async(req,res) =>{
-    try{
-    const usr= new Usr({
-        _id:req.body._id,
-        usr_id:req.body.usr_id,
-        first_name:req.body.first_name,
-        middle_name:req.body.middle_name,
-        last_name:req.body.last_name,
-        email:req.body.email,
-        account_active:req.body.account_active,
-        create_ts:req.body.create_ts,
-        created_by:req.body.created_by,
-        update_ts:req.body.update_ts,
-        modified_by:req.body.modified_by
-    });
+// const usr_update= async(req,res) =>{
+//     try{
+//     const usr= new Usr({
+//         _id:req.body._id,
+//         usr_id:req.body.usr_id,
+//         first_name:req.body.first_name,
+//         middle_name:req.body.middle_name,
+//         last_name:req.body.last_name,
+//         email:req.body.email,
+//         account_active:req.body.account_active,
+//         create_ts:req.body.create_ts,
+//         created_by:req.body.created_by,
+//         update_ts:req.body.update_ts,
+//         modified_by:req.body.modified_by
+//     });
     
-        const updateUsr= await Usr.findByIdAndUpdate(
-            {_id:req.params.usr_id}
-            ,usr
-        );
-        res.json(updateUsr)
-    }catch(error){
-        res.status(400).send(error);
-        console.log({message : error});
-    }
-};
+//         const updateUsr= await Usr.findByIdAndUpdate(
+//             {_id:req.params.usr_id}
+//             ,usr
+//         );
+//         res.json(updateUsr)
+//     }catch(error){
+//         res.status(400).send(error);
+//         console.log({message : error});
+//     }
+// };
 
 module.exports={usr_all
 ,usr_one
